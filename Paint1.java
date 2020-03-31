a71254e7026c2479813af7cd62ce39a333e18332
Neelima Patnaik <51104846+neelup84@users.noreply.github.com>
2020-03-31 13:12:16 -0500

    Add files via upload

---
+++ Paint1.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Paint1 {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double wallHeight = 0.0;
+        double wallWidth = 0.0;
+        double wallArea = 0.0;
+        double gallonsPaintNeeded = 0.0;
+        
+        final double squareFeetPerGallons = 350.0;
+        
+        // Implement a do-while loop to ensure input is valid
+        // Prompt user to input wall's height
+        
+        while(true) {
+        	
+        
+             System.out.println("Enter wall height (feet): ");
+             wallHeight = scnr.nextDouble();
+             if(wallHeight > 0) {
+            	 break;	 
+             }
+             else {
+            	 System.out.println("Enter the valid no...");
+             }
+        }
+        while(true) {
+        	System.out.println("Enter the wall width (feet): ");
+        	wallWidth = scnr.nextDouble();
+        	if(wallWidth > 0) {
+        		break;
+        	}
+        	else {
+        		System.out.println("Enter the valid no...");
+        		
+        	}
+        }
+        
+     
+        // Calculate and output wall area
+        
+        wallArea = wallHeight * wallWidth;
+        System.out.println("Wall area: " + wallArea+ " square feet");
+
+        // Calculate and output the amount of paint (in gallons) needed to paint the wall
+        gallonsPaintNeeded = wallArea/squareFeetPerGallons;
+        System.out.println("Paint needed: " + gallonsPaintNeeded + " gallons");
+
+    }
+}
